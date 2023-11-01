965d9c80e4148f0cdc0a33bfd896df5b5c00f883
Dennis Hickox <frozzel@me.com>
2023-11-01 12:07:56 -0400

    stripe integration finished

--- src/form/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Title({children}) {
-  return (
-    <h1 className="text-xl font-semibold text-center">
-            {children}
-          </h1>
-  )
-}
